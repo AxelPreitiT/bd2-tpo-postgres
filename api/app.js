09e88e26784998cf690e2b80cd7c0c199f99872d
JoseMenta <jrmenta2@gmail.com>
2023-10-04 16:57:01 -0300

    add docker compose configuration

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.listen(8000, () => {
-    console.log('Server running on port 8000');
+    console.log('Server running on port 8000 MAN');
 });
 
 app.get("/", (req, res) => {
