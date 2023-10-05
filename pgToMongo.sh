a5cf3396c209b545682db930cc032290e8c22e54
Axel Preiti Tasat <axel.preitit@gmail.com>
2023-10-04 21:49:04 -0300

    Create shell script for migration

---
+++ pgToMongo.sh
@@ -0,0 +1,4 @@
+docker exec test-pg psql -d postgres -U postgres -p 5432 -c "\copy (select nro_cliente AS \"nro_cliente.int32()\", nombre as \"nombre.auto()\", apellido as \"apellido.auto()\", direccion as \"direccion.auto()\", activo as \"activo.int32()\" FROM e01_cliente) to /tmp/cliente.tsv header delimiter e'\t' csv;"
+docker cp test-pg:/tmp/cliente.tsv .
+docker cp ./cliente.tsv test-mongo:/tmp/cliente.tsv
+docker exec test-mongo mongoimport --collection cliente --type tsv --file /tmp/cliente.tsv --headerline --columnsHaveTypes
